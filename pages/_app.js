b35b47cae928b04987b0434e53a428a1ff67c3bb
tjdgus3160 <tjdgus3160@gmail.com>
2021-04-28 20:38:02 +0900

    Establish layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
+import LeftSideBar from '../src/components/LeftSideBar'
+import RightSideBar from '../src/components/RightSideBar'
+import Top from '../src/components/Top'
 import '../styles/globals.css'
+import styled from 'styled-components'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const Aurora = ({ Component, pageProps }) => {
+  return (
+    <Wrapper>
+      <Top />
+      <Main>
+        <LeftSideBar />
+        <MainComponent>
+          <Component {...pageProps} />
+        </MainComponent>
+        <RightSideBar />
+      </Main>
+    </Wrapper>
+  )
 }
 
-export default MyApp
+const Wrapper = styled.div`
+  display : flex;
+  flex-direction: column;
+  height: 100%;
+  width: 1200px;
+  margin: 0 auto;
+`
+
+const Main = styled.div`
+  display : flex;
+  flex : 9 1 0;
+`
+
+const MainComponent = styled.div`
+  display : flex;
+  flex : 4 1 0;
+`
+
+export default Aurora
---
+++ src/components/LeftSideBar.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const LeftSideBar = () => {
+  return <Wrapper>LeftSideBar</Wrapper>
+}
+
+const Wrapper = styled.div`
+    flex : 1 1 0;
+    border: 1px solid black;
+`
+
+export default LeftSideBar
