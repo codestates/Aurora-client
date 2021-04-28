4841cc3fedefcc0de6ba36d73ebfd8594273206a
tjdgus3160 <tjdgus3160@gmail.com>
2021-04-28 17:30:44 +0900

    create dev branch

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
-  const a = 10
-  console.log(10===11)
   return (
     <div>Hello World</div>
   )
