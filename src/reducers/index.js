d4eb6ce57727619d81b10929c6fa9deb7a359db0
Gahee Kim <rkgml981105@gmail.com>
2021-04-29 15:30:15 +0900

    create redux store, reducers & install redux-thunk, next-redux-wrapper

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { HYDRATE } from 'next-redux-wrapper'
+import { combineReducers } from 'redux'
+
+import user from './userReducer'
+
+// (이전상태, 액션) => 다음상태
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      console.log('HYDRATE', action)
+      return action.payload
+    default: {
+      const combinedReducer = combineReducers({
+        user
+      })
+      return combinedReducer(state, action)
+    }
+  }
+}
+
+export default rootReducer
